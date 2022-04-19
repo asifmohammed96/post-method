3afe5c8efd164e105ad311e22aa99353144df1c1
asifmohammed96 <93712611+asifmohammed96@users.noreply.github.com>
2022-04-19 10:05:44 +0530

    Add files via upload

---
+++ postcontroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class postcontroller extends Controller
+{
+    public function addpost(){
+        return view('addpost');
+    }
+    public function savepost(){
+
+    }
+}
